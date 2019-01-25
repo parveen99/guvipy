04d833f9aa4069b19461d62f8a9eb74b7cc03ea8
parveen99 <40237237+parveen99@users.noreply.github.com>
2019-01-25 20:04:12 +0530

    Update numtype(set1).py

--- numtype(set1).py
+++ numtype(set1).py
@@ -1,7 +1,8 @@
 xo1=int(input())
-if xo1==0:
-    print("Zero")
-elif xo1<0:
-    print("Negative")
-else:
-    print("Positive")
+if(1>=xo1<=100000):
+    if xo1==0:
+        print("Zero")
+    elif xo1<0:
+        print("Negative")
+    else:
+        print("Positive")
